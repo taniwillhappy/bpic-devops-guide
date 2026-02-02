01bc927c6b8d088c37e5c5a1ca8ff1729d8be3b2
taniwillhappy <chidewch@gmail.com>
2026-02-02 14:07:02 +0700

    Enable strict mode in router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,7 @@ import AboutView from '@/views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  strict: true,
   routes: [
     { path: '/', component: dev },
     { path: '/nodejs_ubuntu', component: NodejsUbuntu},
