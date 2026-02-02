6c82e4863669847505f4bbbf0e5ca0526372b9fe
taniwillhappy <chidewch@gmail.com>
2026-02-01 22:39:56 -0800

    Update FileDownload.vue

--- src/views/FileDownload.vue
+++ src/views/FileDownload.vue
@@ -4,7 +4,7 @@
 <template>
   <!-- หัวข้อ  -->
   <h1>Download BPIC Web Project</h1><br>
-  <p><a href="../upload/smartclass-bpic.rar" download>คลิกเพื่อดาวโหลดไฟล์งาน Project</a></p>
+  <p><a href="https://drive.usercontent.google.com/download?id=1t5M2vgMnrgH6V69x1sro_fCAJx5EaovV&export=download&authuser=0&confirm=t&uuid=bcbddef6-e33c-40dc-82a1-39f09b1fda51&at=APcXIO3ZGlYxsVof29iOyyzTmIfh:1770014193510">คลิกเพื่อดาวโหลดไฟล์งาน Project</a></p>
   <br><br>
   <hr>
   <br>
